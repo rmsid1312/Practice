ff1c25b5f33bcd135a7edbc5c9c19ec98df9ca69
rmsid1312 <37926663+rmsid1312@users.noreply.github.com>
2019-03-15 16:29:25 +0900

    add

--- src/_1120.java
+++ src/_1120.java
@@ -10,7 +10,7 @@ public class _1120 {
 			sum=0;
 			for(int j=0;j<a.length();j++) {
 				if(a.charAt(j) != b.charAt(j+i)) sum++;
-			}
+			}			
 			min=Math.min(min, sum);
 		}
 		System.out.println(min);
